bc611bccc559404830e9ea02fe847f0746abffd5
swresca <kwoo111228@gmail.com>
2024-03-27 11:46:20 +0900

    3~4주차 과제

---
+++ 0320/0321-1/0327.cpp
@@ -0,0 +1,61 @@
+﻿//과제물 Lecture04_HW
+//idel : 화면 검정색으로 clear
+//마우스 오른쪽으로 key down하면 :빨간색
+//마우스 오른쪽으로 key up하면 :원상복구
+// //마우스 왼쪽로 key down하면 :녹색
+//마우스 오른쪽으로 key up하면 :원상복구
+// 마우스 오른쪽으로 key down 하고 드래그 중이면 :파란색
+// 마우스 왼쪽 key down 하고 드래그 중이면 마젠타색  
+ 
+#pragma comment(lib,"Opengl32.lib")
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+void errorCallback(int error, const char* description)
+{
+    std::cerr << "GLFW Error : " << description << std::endl;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+int main(void)
+{
+    // GLFW 라이브러리 초기화
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1280, 768, "swresca", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    glfwSetErrorCallback(errorCallback);
+    glfwSetKeyCallback(window, keyCallback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClearColor(1.0f, 0.0f, 1.0f, 1.0f); // 검정색으로 클리어
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw code here
+
+        glfwSwapBuffers(window);
+        
+    }
+
+    glfwTerminate();
+    return 0;
+}
---
+++ 0320/lecture04_HW/lecture04_HW.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#pragma comment(lib, "opengl32.lib")
+#include <GLFW/glfw3.h>
+
+void errorCallback(int error, const char* description)
+{
+    std::cerr << "GLFW Error: " << description << std::endl;
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // 빨간색
+    else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // 검정색
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+        glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // 녹색
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // 검정색
+}
+
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // 파랑색
+    else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+        glClearColor(1.0f, 0.0f, 1.0f, 1.0f); // 마젠타색
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main(void)
+{
+    // glfw 라이브러리 초기화
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1280, 768, "MuSoeunEngine", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    glfwSetErrorCallback(errorCallback);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        // 마우스를 먹게 만들어 준당 개꿀
+        glfwPollEvents();
+
+        // 버퍼를 초기화 시킨다.
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
